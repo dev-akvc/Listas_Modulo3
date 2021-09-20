c2402e18a45f2e284e7d87189d00c66334b1c651
Anna Karolyne <dev.akvc@gmail.com>
2021-09-20 01:23:24 -0300

    Exercício 2

--- Lista1/src/br/com/zup/Bola.java
+++ Lista1/src/br/com/zup/Bola.java
@@ -1,2 +1,13 @@
-package br.com.zup;public class Bola {
+package br.com.zup;
+
+public class Bola {
+    double circunferencia, velocidade;
+    String cor, marca;
+
+    public Bola(String cor, String marca, double circunferencia, double velocidade){
+        this.cor = cor;
+        this.marca = marca;
+        this.circunferencia = circunferencia;
+        this.velocidade = velocidade;
+    }
 }
