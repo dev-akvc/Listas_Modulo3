a83a2ff2111a532417f8bb95bb19839e978b0602
Anna Karolyne <dev.akvc@gmail.com>
2021-09-20 20:59:46 -0300

    Exercício 1- Classe Carro

--- ListaModelagemDeClasses/src/br/com/zup/Exercicio1/Carro.java
+++ ListaModelagemDeClasses/src/br/com/zup/Exercicio1/Carro.java
@@ -1,4 +1,31 @@
 package br.com.zup.Exercicio1;
 
 public class Carro {
+    String nome, fabricante, cor;
+    double peso, valor;
+
+    //    Construtor
+    public Carro(String nome, String fabricante, String cor, double peso, double valor) {
+        this.nome = nome;
+        this.fabricante = fabricante;
+        this.cor = cor;
+        this.peso = peso;
+        this.valor = valor;
+    }
+
+    // Método para calcular desconto
+    public void calcularDesconto(double valorDesconto) {
+        valor = valor - valorDesconto;
+    }
+
+    // Método para pintar
+    public void pintarCarro(String novaCor) {
+        cor = novaCor;
+    }
+
+    // Imprimir informações do carro
+    public void mostrarCarro() {
+        System.out.print("O carro " + nome +" " +cor + ", do fabricante " + fabricante);
+        System.out.println(", pesa " +peso+ "kg e custa R$ " +valor);
+    }
 }
--- ListaModelagemDeClasses/src/br/com/zup/Exercicio1/Main.java
+++ ListaModelagemDeClasses/src/br/com/zup/Exercicio1/Main.java
@@ -1,4 +1,19 @@
 package br.com.zup.Exercicio1;
 
 public class Main {
+    public static void main(String[] args) {
+        //    Instanciando o carro
+        Carro veloster = new Carro("Veloster", "Hyundai", "preto", 1000, 11111.11);
+
+        //    Exibindo informações do carro
+        veloster.mostrarCarro();
+
+        //    Calculando desconto
+        veloster.calcularDesconto(200);
+        veloster.mostrarCarro();
+
+        //    Pintando carro
+        veloster.pintarCarro("rosa pink");
+        veloster.mostrarCarro();
+    }
 }
