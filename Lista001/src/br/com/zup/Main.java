7305bfdf647f4efdf99a9743e95d54cc37254ad1
Anna Karolyne <dev.akvc@gmail.com>
2021-09-20 03:04:11 -0300

    Level Up - Continente

--- Lista001/src/br/com/zup/Main.java
+++ Lista001/src/br/com/zup/Main.java
@@ -1,5 +1,7 @@
 package br.com.zup;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,12 +9,12 @@ public class Main {
         Bola colorida = new Bola("rosa e preto", "flash", 2.3, 10);
         Bola futebol = new Bola("branca", "centauro", 3, 24.5);
 
-        System.out.println("A cor da bola é " +colorida.cor+ ", a marca " +colorida.marca+ ", circunferência " +colorida.circunferencia+ " e velocidade " +colorida.velocidade);
+        System.out.println("A cor da bola é " + colorida.cor + ", a marca " + colorida.marca + ", circunferência " + colorida.circunferencia + " e velocidade " + colorida.velocidade);
 
-        System.out.print("A cor da bola é " +futebol.cor);
-        System.out.print(", a marca " +futebol.marca);
-        System.out.print(", circunferência " +futebol.circunferencia);
-        System.out.println(" e velocidade " +futebol.velocidade);
+        System.out.print("A cor da bola é " + futebol.cor);
+        System.out.print(", a marca " + futebol.marca);
+        System.out.print(", circunferência " + futebol.circunferencia);
+        System.out.println(" e velocidade " + futebol.velocidade);
 
         System.out.println("==========================================");
 
@@ -21,8 +23,8 @@ public class Main {
         Pessoa homem = new Pessoa("Giovane", 37, 1.7);
         homem.peso = 60;
 
-        System.out.println("O nome é " +mulher.nome+ ", a idade " +mulher.idade+ " anos, e altura " +mulher.altura);
-        System.out.println("O nome é " +homem.nome+ ", a idade " +homem.idade+ " anos, altura " +homem.altura+ "m e peso " +homem.peso+ "kg");
+        System.out.println("O nome é " + mulher.nome + ", a idade " + mulher.idade + " anos, e altura " + mulher.altura);
+        System.out.println("O nome é " + homem.nome + ", a idade " + homem.idade + " anos, altura " + homem.altura + "m e peso " + homem.peso + "kg");
 
         System.out.println("==========================================");
 
@@ -32,8 +34,8 @@ public class Main {
         ContaCorrente fimDeMes = new ContaCorrente("Karol Pobre", "87432-7");
         fimDeMes.saldo = -50.2;
 
-        System.out.println("A cliente " +comecoDeMes.nome+ " titular da conta " +comecoDeMes.numeroDaConta+ " começa o mês com o saldo de R$" + comecoDeMes.saldo);
-        System.out.println("Já no fim do mês o saldo é de R$" +fimDeMes.saldo);
+        System.out.println("A cliente " + comecoDeMes.nome + " titular da conta " + comecoDeMes.numeroDaConta + " começa o mês com o saldo de R$" + comecoDeMes.saldo);
+        System.out.println("Já no fim do mês o saldo é de R$" + fimDeMes.saldo);
 
         System.out.println("==========================================");
 
@@ -42,9 +44,49 @@ public class Main {
         Carro veloster = new Carro("Hyundai", "Veloster", "3");
         veloster.potencia = 10.2;
 
-        System.out.println(kwid.modelo+ " é da marca " +kwid.marca);
-        System.out.println("Veloster tem " +veloster.potencia+ " de potência");
+        System.out.println(kwid.modelo + " é da marca " + kwid.marca);
+        System.out.println("Veloster tem " + veloster.potencia + " de potência");
 
         System.out.println("==========================================");
+
+// LEVEL UP - Instanciando objetos da classe País
+        Scanner entrada = new Scanner(System.in);
+
+//        Criando lista
+        Map<String, String> continente = new HashMap<>();
+        boolean opcaoEscolhida = true;
+
+        Pais autralia = new Pais("Austrália", "extensão 7.713.364km²", "21.292.893 habitantes");
+        Pais fiji = new Pais("Fiji", "extensão 18.270 km²", "889.953 habitantes");
+        Pais novazelandia = new Pais("Nova Zelândia", "extensão 268.107 km²", "4.917.534 habitantes");
+        Pais tonga = new Pais("Tonga", "extensão 748km²", "103.800 habitantes");
+
+//         Adicionando os países à lista
+        continente.put(autralia.nome, autralia.extensaoTerritorial + autralia.populacaoTotal);
+        continente.put(fiji.nome, fiji.extensaoTerritorial + fiji.populacaoTotal);
+        continente.put(novazelandia.nome, novazelandia.extensaoTerritorial + novazelandia.populacaoTotal);
+        continente.put(tonga.nome, tonga.extensaoTerritorial + tonga.populacaoTotal);
+
+        String menuInicial = "\nDigite:\n1- Exibir menu\n2- Sair do programa\n";
+        System.out.println(menuInicial);
+        int opcao = entrada.nextInt();
+
+        while(opcaoEscolhida) {
+        if(opcao == 1){
+            System.out.println("Lista do continente Oceania\n"+continente.toString());
+            System.out.println(menuInicial);
+            opcao = entrada.nextInt();
+        }
+        if(opcao == 2){
+            System.out.println("Programa encerrado.");
+            opcaoEscolhida = false;
+        }
+        else {
+            System.out.println("Opção inválida.");
+            System.out.println(menuInicial);
+            opcao = entrada.nextInt();
+        }
+
+        }
     }
 }
---
+++ Lista001/src/br/com/zup/Pais.java
@@ -0,0 +1,11 @@
+package br.com.zup;
+
+public class Pais {
+    String nome, extensaoTerritorial, populacaoTotal;
+
+    public Pais(String nome, String extensaoTerritorial, String populacaoTotal){
+        this.nome = nome;
+        this.extensaoTerritorial = extensaoTerritorial;
+        this.populacaoTotal = populacaoTotal;
+    }
+}
