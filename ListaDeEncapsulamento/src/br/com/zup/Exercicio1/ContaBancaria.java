a09cb6c1f19257005e9d2dca21be27dc23b382f0
Anna Karolyne <dev.akvc@gmail.com>
2021-09-21 23:50:01 -0300

    Exercício 1 - pendente revisão

--- ListaDeEncapsulamento/src/br/com/zup/Exercicio1/ContaBancaria.java
+++ ListaDeEncapsulamento/src/br/com/zup/Exercicio1/ContaBancaria.java
@@ -1,4 +1,55 @@
 package br.com.zup.Exercicio1;
 
 public class ContaBancaria {
+    //    Criando atributos da Classe
+    String nomeCorrentista;
+    String numeroConta;
+    double saldoConta;
+
+    //    Criando construtor
+    public ContaBancaria() {
+
+    }
+
+    public ContaBancaria(String nomeCorrentista, String numeroConta, double saldoConta) {
+        this.nomeCorrentista = nomeCorrentista;
+        this.numeroConta = numeroConta;
+        this.saldoConta = saldoConta;
+    }
+
+    //    Criando métodos
+//    public String getNomeCorrentista() {
+//        return nomeCorrentista;
+//    }
+
+    public void setNomeCorrentista(String nomeCorrentista) {
+        this.nomeCorrentista = nomeCorrentista;
+    }
+
+//    public String getNumeroConta() {
+//        return numeroConta;
+//    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+//    public double getSaldoConta() {
+//        return saldoConta;
+//    }
+
+    public void depositar(double deposito){
+        saldoConta += deposito;
+    }
+
+    public void sacar(double saque){
+        saldoConta -= saque;
+    }
+
+    public void extrato(){
+        System.out.println("Nome do correntista é " +nomeCorrentista);
+        System.out.println("Número da conta é " +numeroConta);
+        System.out.println("Saldo da conta R$" +saldoConta);
+    }
+
 }
--- ListaDeEncapsulamento/src/br/com/zup/Exercicio1/Main.java
+++ ListaDeEncapsulamento/src/br/com/zup/Exercicio1/Main.java
@@ -2,6 +2,18 @@ package br.com.zup.Exercicio1;
 
 public class Main {
     public static void main(String[] args) {
+        ContaBancaria itau = new ContaBancaria ();
+
+        itau.extrato();
+        itau.setNomeCorrentista("Karol Souza");
+        itau.setNumeroConta("1256-9");
+        itau.extrato();
+
+        itau.depositar(15000);
+        itau.extrato();
+
+        itau.sacar(1000.5);
+        itau.extrato();
 
     }
 }
