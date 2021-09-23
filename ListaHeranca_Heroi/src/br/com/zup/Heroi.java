3214d036f30205f74442ac121794581cc1935212
Anna Karolyne <dev.akvc@gmail.com>
2021-09-23 20:52:44 -0300

    Classe Heroi

--- ListaHeranca_Heroi/src/br/com/zup/Heroi.java
+++ ListaHeranca_Heroi/src/br/com/zup/Heroi.java
@@ -1,6 +1,33 @@
 package br.com.zup;
 
 public class Heroi {
+    private String nome;
+    private double vida;
 
+//    Métodos construtores
+    public Heroi() {
+    }
 
+    public Heroi(String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+//    Métodos solicitados
+    public void trocarNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void receberDano(double dano) {
+        this.vida -= dano;
+    }
+
+    public double exibirVida() {
+        return this.vida;
+    }
+
+//    Getter que falta
+    public String getNome() {
+        return nome;
+    }
 }
