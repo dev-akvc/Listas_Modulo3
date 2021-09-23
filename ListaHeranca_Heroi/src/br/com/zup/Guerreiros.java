0200df7c2132a4aba2fc61c3bb6556a5f7e0c901
Anna Karolyne <dev.akvc@gmail.com>
2021-09-23 13:45:43 -0300

    Classe Guerreiros (+correção dos métodos para públicos)

--- ListaHeranca_Heroi/src/br/com/zup/Guerreiros.java
+++ ListaHeranca_Heroi/src/br/com/zup/Guerreiros.java
@@ -1,4 +1,21 @@
 package br.com.zup;
 
 public class Guerreiros extends Heroi{
+    private double poderAtaque;
+
+    public Guerreiros(String nome, double vida) {
+        super(nome, vida);
+    }
+
+    public double aplicarDano(){
+        return poderAtaque;
+    }
+
+    public double getPoderAtaque() {
+        return poderAtaque;
+    }
+
+    public void setPoderAtaque(double poderAtaque) {
+        this.poderAtaque = poderAtaque;
+    }
 }
