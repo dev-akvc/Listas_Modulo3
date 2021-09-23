a59a44f4a58fa0354b99d85a78d41339ab1d4cd7
Anna Karolyne <dev.akvc@gmail.com>
2021-09-23 13:38:45 -0300

    Classe Magos

--- ListaHeranca_Heroi/src/br/com/zup/Magos.java
+++ ListaHeranca_Heroi/src/br/com/zup/Magos.java
@@ -1,6 +1,22 @@
 package br.com.zup;
 
 public class Magos extends Heroi{
+    private double poderMagico;
 
+    public Magos(String nome, double vida, double poderMagico) {
+        super(nome, vida);
+        this.poderMagico = poderMagico;
+    }
 
+    private double aplicarMagia(){
+        return poderMagico;
+    }
+
+    public double getPoderMagico() {
+        return poderMagico;
+    }
+
+    public void setPoderMagico(double poderMagico) {
+        this.poderMagico = poderMagico;
+    }
 }
