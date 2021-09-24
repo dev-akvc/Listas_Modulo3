6c6874d92b1254081f299125ae4b127f2e7abfd6
Anna Karolyne <dev.akvc@gmail.com>
2021-09-24 00:35:39 -0300

    Classe Fornecedor

--- ListaClasses_Colaboradores/src/br/com/zup/Fornecedor.java
+++ ListaClasses_Colaboradores/src/br/com/zup/Fornecedor.java
@@ -1,8 +1,37 @@
 package br.com.zup;
 
-public class Fornecedor extends Pessoa{
+public class Fornecedor extends Pessoa {
     private double valorCredito;
     private double valorDivida;
 
     //    Métodos construtores
+    public Fornecedor() {
+    }
+
+    public Fornecedor(String nome, String endereco, String telefone, double valorCredito, double valorDivida) {
+        super(nome, endereco, telefone);
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+    }
+
+    //    Getters e Setters
+    public double getValorCredito() {
+        return valorCredito;
+    }
+
+    public void setValorCredito(double valorCredito) {
+        this.valorCredito = valorCredito;
+    }
+
+    public double getValorDivida() {
+        return valorDivida;
+    }
+
+    public void setValorDivida(double valorDivida) {
+        this.valorDivida = valorDivida;
+    }
+
+    public void obterSaldo(){
+        valorCredito -= valorDivida;
+    }
 }
