3658f36b70891906ed20f6c702e48d2e34027ff4
Anna Karolyne <dev.akvc@gmail.com>
2021-09-23 22:05:06 -0300

    Classe Inimigos

--- ListaHeranca_Heroi/src/br/com/zup/Inimigos.java
+++ ListaHeranca_Heroi/src/br/com/zup/Inimigos.java
@@ -1,4 +1,30 @@
 package br.com.zup;
 
 public class Inimigos {
+    private String nome;
+    private double vida;
+
+    public Inimigos(String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    //     Métodos solicitados
+    public void trocarNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void receberDano(double dano) {
+        this.vida -= dano;
+    }
+
+    public double exibirVida() {
+        return this.vida;
+    }
+
+    //    Getter que falta
+
+    public String getNome() {
+        return nome;
+    }
 }
