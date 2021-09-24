5329df0f0c868b7c4113d8e7aaaf6336f3495bbe
Anna Karolyne <dev.akvc@gmail.com>
2021-09-23 21:46:02 -0300

    Classe Guerreiros

--- ListaHeranca_Heroi/src/br/com/zup/Guerreiros.java
+++ ListaHeranca_Heroi/src/br/com/zup/Guerreiros.java
@@ -1,5 +1,24 @@
 package br.com.zup;
 
-public class Guerreiros{
+public class Guerreiros extends Heroi {
+    private double poderAtaque;
 
+    //    Métodos construtores
+    public Guerreiros() {
+    }
+
+    public Guerreiros(String nome, double vida, double poderAtaque) {
+        super(nome, vida);
+        this.poderAtaque = poderAtaque;
+    }
+
+    //    Método solicitado
+    public double aplicarDano() {
+        return poderAtaque;
+    }
+
+    //    Setter que falta
+    public void setPoderAtaque(double poderAtaque) {
+        this.poderAtaque = poderAtaque;
+    }
 }
