142d2532aafd17ce1a595bf1c57b9399546537d3
Anna Karolyne <dev.akvc@gmail.com>
2021-09-24 01:08:50 -0300

    Classe Colaborador

--- ListaClasses_Colaboradores/src/br/com/zup/Colaborador.java
+++ ListaClasses_Colaboradores/src/br/com/zup/Colaborador.java
@@ -1,4 +1,48 @@
 package br.com.zup;
 
-public class Colaborador {
+public class Colaborador extends Pessoa{
+    private int codigoSetor;
+    private double salarioBase;
+    private double imposto;
+
+    //    Métodos construtores
+    public Colaborador() {
+    }
+
+    public Colaborador(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto) {
+        super(nome, endereco, telefone);
+        this.codigoSetor = codigoSetor;
+        this.salarioBase = salarioBase;
+        this.imposto = imposto;
+    }
+
+//    Getters e setters
+    public int getCodigoSetor() {
+        return codigoSetor;
+    }
+
+    public void setCodigoSetor(int codigoSetor) {
+        this.codigoSetor = codigoSetor;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public double getImposto() {
+        return imposto;
+    }
+
+    public void setImposto(double imposto) {
+        this.imposto = imposto;
+    }
+
+    public double calcularSalario(){
+        double salarioLiquido = salarioBase - imposto;
+        return salarioLiquido;
+    }
 }
--- ListaClasses_Colaboradores/src/br/com/zup/Main.java
+++ ListaClasses_Colaboradores/src/br/com/zup/Main.java
@@ -4,14 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         Fornecedor fornecedor = new Fornecedor("Giovane", "Av Paralela, nº27", "(53)99345-6783", 5000, 1000);
+        Colaborador colaborador = new Colaborador("Bruno", "Rua das Bandeira, S/N", "(13)93246-9321",1, 7000, 100);
 
         //        Teste saldo do fornecedor
         System.out.println("O saldo é R$ " +fornecedor.obterSaldo());
         fornecedor.setValorCredito(6000);
         System.out.println("O saldo é R$ " +fornecedor.obterSaldo());
 
-        //        Teste endereço
+        //        Teste endereço do fornecedor
         System.out.println("O endereço de " +fornecedor.getNome() +" é " +fornecedor.getEndereco());
+
+        //        Teste colaborador
+        System.out.println("O salário de " +colaborador.getNome() +" é R$ " +colaborador.calcularSalario());
+
+
 
 
 
