909f98430b0b01856b69f109e3d60c9d88e3aefb
Anna Karolyne <dev.akvc@gmail.com>
2021-09-26 18:47:50 -0300

    Classe Professor - Métodos Contrutores, Getters e Setters

--- ListaClasses_Universidade/src/br/com/zup/Professor.java
+++ ListaClasses_Universidade/src/br/com/zup/Professor.java
@@ -1,4 +1,55 @@
 package br.com.zup;
 
-public class Professor {
+public class Professor extends Funcionario{
+    private String nivelGraduacao;
+    private String disciplina;
+    private int qtdAlunos;
+    private int qtdTurmas;
+
+    //    Métodos construtores
+
+    public Professor() {
+    }
+
+    public Professor(String nome, String cpf, double numeroDeRegistro, String orgaoLotacao, double salario, String nivelGraduacao, String disciplina, int qtdAlunos, int qtdTurmas) {
+        super(nome, cpf, numeroDeRegistro, orgaoLotacao, salario);
+        this.nivelGraduacao = nivelGraduacao;
+        this.disciplina = disciplina;
+        this.qtdAlunos = qtdAlunos;
+        this.qtdTurmas = qtdTurmas;
+    }
+
+    //    Getters e setters
+
+    public String getNivelGraduacao() {
+        return nivelGraduacao;
+    }
+
+    public void setNivelGraduacao(String nivelGraduacao) {
+        this.nivelGraduacao = nivelGraduacao;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public int getQtdAlunos() {
+        return qtdAlunos;
+    }
+
+    public void setQtdAlunos(int qtdAlunos) {
+        this.qtdAlunos = qtdAlunos;
+    }
+
+    public int getQtdTurmas() {
+        return qtdTurmas;
+    }
+
+    public void setQtdTurmas(int qtdTurmas) {
+        this.qtdTurmas = qtdTurmas;
+    }
 }
