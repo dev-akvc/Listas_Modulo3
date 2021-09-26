11222b808597c26fe8cc183685795e0a85b24fb5
Anna Karolyne <dev.akvc@gmail.com>
2021-09-26 19:04:21 -0300

    Classe Funcionário ADM - Métodos Contrutores, Getters e Setters

--- ListaClasses_Universidade/src/br/com/zup/FuncionarioAdm.java
+++ ListaClasses_Universidade/src/br/com/zup/FuncionarioAdm.java
@@ -1,4 +1,33 @@
 package br.com.zup;
 
-public class FuncionarioAdm {
+public class FuncionarioAdm extends Funcionario{
+    private String funcaoAdministrativa;
+    private String senioridade;
+
+    //    Métodos construtores
+    public FuncionarioAdm() {
+    }
+
+    public FuncionarioAdm(String nome, String cpf, double numeroDeRegistro, String orgaoLotacao, double salario, String funcaoAdministrativa, String senioridade) {
+        super(nome, cpf, numeroDeRegistro, orgaoLotacao, salario);
+        this.funcaoAdministrativa = funcaoAdministrativa;
+        this.senioridade = senioridade;
+    }
+
+    //    Getters e setters
+    public String getFuncaoAdministrativa() {
+        return funcaoAdministrativa;
+    }
+
+    public void setFuncaoAdministrativa(String funcaoAdministrativa) {
+        this.funcaoAdministrativa = funcaoAdministrativa;
+    }
+
+    public String getSenioridade() {
+        return senioridade;
+    }
+
+    public void setSenioridade(String senioridade) {
+        this.senioridade = senioridade;
+    }
 }
