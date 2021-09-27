b4b2f1edc67be51e94690ff3622feb679449e9d8
Anna Karolyne <dev.akvc@gmail.com>
2021-09-27 16:46:12 -0300

    Classe Morador - Métodos construtores, getters e setters

--- ListaClasses_Imobiliaria/src/br/com/zup/Morador.java
+++ ListaClasses_Imobiliaria/src/br/com/zup/Morador.java
@@ -1,4 +1,54 @@
 package br.com.zup;
 
 public class Morador {
+    private String nome;
+    private String cpf;
+    private String ocupacao;
+    private boolean animalDeEstimacao = true;
+
+    //    Métodos construtores
+    public Morador() {
+    }
+
+    public Morador(String nome, String cpf, String ocupacao, boolean animalDeEstimacao) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.ocupacao = ocupacao;
+        this.animalDeEstimacao = animalDeEstimacao;
+    }
+
+    //    Getters e setters
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getOcupacao() {
+        return ocupacao;
+    }
+
+    public void setOcupacao(String ocupacao) {
+        this.ocupacao = ocupacao;
+    }
+
+    public boolean isAnimalDeEstimacao() {
+        return animalDeEstimacao;
+    }
+
+    public void setAnimalDeEstimacao(boolean animalDeEstimacao) {
+        this.animalDeEstimacao = animalDeEstimacao;
+    }
 }
