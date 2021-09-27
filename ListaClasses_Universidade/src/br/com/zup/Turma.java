68efb52099d6e9bd23264c0f858a8dd936c204c9
Anna Karolyne <dev.akvc@gmail.com>
2021-09-26 21:38:40 -0300

    Classe Turma - Métodos Contrutores, Getters e Setters

--- ListaClasses_Universidade/src/br/com/zup/Turma.java
+++ ListaClasses_Universidade/src/br/com/zup/Turma.java
@@ -1,5 +1,32 @@
 package br.com.zup;
 
 public class Turma {
+    private int qtdAlunos;
+    private String serie;
 
+    //    Métodos construtores
+    public Turma() {
+    }
+
+    public Turma(int qtdAlunos, String serie) {
+        this.qtdAlunos = qtdAlunos;
+        this.serie = serie;
+    }
+
+    //    Getters e setters
+    public int getQtdAlunos() {
+        return qtdAlunos;
+    }
+
+    public void setQtdAlunos(int qtdAlunos) {
+        this.qtdAlunos = qtdAlunos;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
 }
