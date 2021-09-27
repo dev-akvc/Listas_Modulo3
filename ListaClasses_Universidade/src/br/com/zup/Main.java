0ae7182d106c25fa5f1ec0c03c4d3fa57cbb2453
Anna Karolyne <dev.akvc@gmail.com>
2021-09-26 21:11:25 -0300

    Criando classe Turma

--- ListaClasses_Universidade/src/br/com/zup/Main.java
+++ ListaClasses_Universidade/src/br/com/zup/Main.java
@@ -6,6 +6,7 @@ public class Main {
         Professor prof = new Professor("Anna", "123.456.789.10", 12987, "Colégio XVI", 3000, "Superior", "Física", 15, 2);
         Coordenador coord = new Coordenador("Karol", "432.515.803-09", 13, "Colégio XVI", 7500, 1);
 
+        //      teste de adicionar professores à classe coordenador
         coord.aumentarSalario();
         System.out.println(coord.getSalario());
         System.out.println(coord.getQtdProfSupervisionados());
