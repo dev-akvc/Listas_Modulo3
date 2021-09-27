2c99246b0f4356c2163f822c1a6b264c52be716a
Anna Karolyne <dev.akvc@gmail.com>
2021-09-27 19:23:45 -0300

    Classe Catálogo - Métodos getters e setters

--- ListaClasses_Imobiliaria/src/br/com/zup/Catalogo.java
+++ ListaClasses_Imobiliaria/src/br/com/zup/Catalogo.java
@@ -1,5 +1,23 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Catalogo {
+    private List<Imovel> listaImoveis = new ArrayList<>();
+    private String menuInicial;
+
+    //    Getters e setters
+    public String getMenuInicial() {
+        return menuInicial;
+    }
+
+    public void setMenuInicial(String menuInicial) {
+        this.menuInicial = menuInicial;
+    }
+
+    public void menuInicial(int opcao) {
+
+    }
 
 }
