c5e7254f93a4e884de776e5fc05d723d268758ff
Anna Karolyne <dev.akvc@gmail.com>
2021-09-28 14:55:00 -0300

    Limpando método Main

--- ListaClasses_Imobiliaria/src/br/com/zup/Main.java
+++ ListaClasses_Imobiliaria/src/br/com/zup/Main.java
@@ -5,30 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        Imovel casa1 = new Imovel("Rua das Araras, nº14", 750, "Anna");
-        Morador morador1 = new Morador("Karol", "325.762.456-89", "Desenvolvedora");
-        Morador morador2 = new Morador("Giovane", "456.457.098-23", "Desenvolvedor");
 
-        Imovel casa2 = new Imovel("Rua dos Pardais, SN", 850, "Anna");
-        Morador morador3 = new Morador("Bruno", "756.235.098-12", "Vendedor", false);
-
-        Catalogo catalogo = new Catalogo();
-//        MenuInicial menu = new MenuInicial();
-
-
-        //        Adicionando moradores nos imóveis
-        casa1.adicionarMoradores(morador1);
-        casa1.adicionarMoradores(morador2);
-        System.out.println(casa1);
-        System.out.println("=========================");
-
-        casa2.adicionarMoradores(morador3);
-        System.out.println(casa2);
-
-//        catalogo.adicionarImoveis(casa1);
-//        catalogo.adicionarImoveis(casa2);
-
-        catalogo.menuInicial();
     }
 }
