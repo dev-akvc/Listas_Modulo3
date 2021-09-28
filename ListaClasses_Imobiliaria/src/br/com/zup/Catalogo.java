74d4525536accab3e9bbbc85332de82083f5a10a
Anna Karolyne <dev.akvc@gmail.com>
2021-09-27 21:02:08 -0300

    Criando classe Menu Inicial

--- ListaClasses_Imobiliaria/src/br/com/zup/Catalogo.java
+++ ListaClasses_Imobiliaria/src/br/com/zup/Catalogo.java
@@ -2,10 +2,14 @@ package br.com.zup;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Catalogo {
+    Scanner entrada = new Scanner(System.in);
     private List<Imovel> listaImoveis = new ArrayList<>();
     private String menuInicial;
+    private boolean opcao = true;
+    private int opcaoEscolhida;
 
     //    Getters e setters
     public String getMenuInicial() {
@@ -16,8 +20,53 @@ public class Catalogo {
         this.menuInicial = menuInicial;
     }
 
-    public void menuInicial(int opcao) {
+    @Override
+    public String toString() {
+        return listaImoveis.toString();
+    }
 
+    public List<Imovel> getListaImoveis() {
+        return listaImoveis;
+    }
+
+    public void setListaImoveis(List<Imovel> listaImoveis) {
+        this.listaImoveis = listaImoveis;
+    }
+
+    public void imprimirLista() {
+        for (Imovel imovel : this.getListaImoveis()) {
+            System.out.println(imovel);
+        }
+    }
+
+    public void menuInicial() {
+        System.out.println("=== Bem vindo(a) ao cadastro de imóveis ===");
+        System.out.println("Digite:\n 1- Exibir imóveis \n 2- Cadastrar imóveis \n 3- Excluir imóveis \n 4- Sair do menu");
+        opcaoEscolhida = entrada.nextInt();
+
+        while (opcao) {
+            switch (opcaoEscolhida) {
+                case 1:
+
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    opcao = false;
+                    break;
+
+                default:
+                    System.out.println("Digite uma opção válida.");
+                    break;
+
+            }
+
+        }
     }
 
 }
--- ListaClasses_Imobiliaria/src/br/com/zup/Main.java
+++ ListaClasses_Imobiliaria/src/br/com/zup/Main.java
@@ -1,14 +1,21 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         Imovel casa1 = new Imovel("Rua das Araras, nº14", 750, "Anna");
         Morador morador1 = new Morador("Karol", "325.762.456-89", "Desenvolvedora");
         Morador morador2 = new Morador("Giovane", "456.457.098-23", "Desenvolvedor");
 
         Imovel casa2 = new Imovel("Rua dos Pardais, SN", 850, "Anna");
         Morador morador3 = new Morador("Bruno", "756.235.098-12", "Vendedor", false);
+
+        Catalogo catalogo = new Catalogo();
+
+        catalogo.menuInicial();
 
         //        Adicionando moradores nos imóveis
         casa1.adicionarMoradores(morador1);
