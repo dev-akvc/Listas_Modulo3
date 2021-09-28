f4445874ea1c27621a1f128bd7f9f2003cb01f87
Anna Karolyne <dev.akvc@gmail.com>
2021-09-28 15:26:22 -0300

    Criando método cadastrar moradores em Sistema

--- ListaClasses_Imobiliaria/src/br/com/zup/Sistema.java
+++ ListaClasses_Imobiliaria/src/br/com/zup/Sistema.java
@@ -1,9 +1,28 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
+
 
     public static void menuInicial (){
         System.out.println("=== Bem vindo(a) ao cadastro de imóveis === \n");
         System.out.println("Digite:\n 1- Exibir imóveis \n 2- Cadastrar imóveis \n 3- Excluir imóveis \n 4- Sair do menu");
+    }
+
+    public static Morador cadastrarMoradores (){
+        String nome = capturarDados("Informe o nome: ").nextLine();
+        String cpf = capturarDados("Informe o CPF: ").nextLine();
+        String ocupacao = capturarDados("Informe a ocupação: ").nextLine();
+        boolean animalDeEstimacao = capturarDados("Tem animal de estimação? ").nextBoolean();
+
+        Morador morador = new Morador(nome, cpf, ocupacao, animalDeEstimacao);
+        return morador;
+    }
+
+    //    Método para automatizar entrada de dados (instancia o Scanner)
+    private static Scanner capturarDados(String mensagem){
+        System.out.println(mensagem);
+        return new Scanner(System.in);
     }
 }
