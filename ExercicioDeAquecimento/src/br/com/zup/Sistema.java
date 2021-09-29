7adaa177386a84fd02bead3807c33514ccf46736
Anna Karolyne <dev.akvc@gmail.com>
2021-09-28 23:44:31 -0300

    Sistema - Método para automatizar Scanner

--- ExercicioDeAquecimento/src/br/com/zup/Sistema.java
+++ ExercicioDeAquecimento/src/br/com/zup/Sistema.java
@@ -1,4 +1,11 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
+    //  Método para automatizar entrada de dados
+    private static Scanner receberDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 }
