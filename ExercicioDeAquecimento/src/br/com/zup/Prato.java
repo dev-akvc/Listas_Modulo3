d863e00cff8f3f5d1717ebbc739895df84f11c65
Anna Karolyne <dev.akvc@gmail.com>
2021-09-28 23:27:08 -0300

    Classe Prato - Métodos construtores, getters e setters

--- ExercicioDeAquecimento/src/br/com/zup/Prato.java
+++ ExercicioDeAquecimento/src/br/com/zup/Prato.java
@@ -1,5 +1,35 @@
 package br.com.zup;
 
-public class Prato {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Prato {
+    private List<Ingrediente> ingredientes = new ArrayList<>();
+    private String nomePrato;
+
+    //  Métodos contrutores
+    public Prato() {
+    }
+
+    public Prato(List<Ingrediente> ingredientes, String nomePrato) {
+        this.ingredientes = ingredientes;
+        this.nomePrato = nomePrato;
+    }
+
+    //  Métodos getters e setters
+    public List<Ingrediente> getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(List<Ingrediente> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public String getNomePrato() {
+        return nomePrato;
+    }
+
+    public void setNomePrato(String nomePrato) {
+        this.nomePrato = nomePrato;
+    }
 }
