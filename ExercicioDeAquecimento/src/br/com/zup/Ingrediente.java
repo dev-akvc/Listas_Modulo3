3b33f2283e5c3febc6011cdefc2a3e2785fffe95
Anna Karolyne <dev.akvc@gmail.com>
2021-09-28 23:09:31 -0300

    Classe Ingrediente - Métodos construtores, getters e setters

--- ExercicioDeAquecimento/src/br/com/zup/Ingrediente.java
+++ ExercicioDeAquecimento/src/br/com/zup/Ingrediente.java
@@ -1,6 +1,34 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ingrediente {
+    private String nome;
+    private List <TabelaNutricional> tabelaNutricional = new ArrayList<>();
 
+    //  Métodos contrutores
+    public Ingrediente() {
+    }
 
+    public Ingrediente(String nome) {
+        this.nome = nome;
+    }
+
+    //  Métodos getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<TabelaNutricional> getTabelaNutricional() {
+        return tabelaNutricional;
+    }
+
+    public void setTabelaNutricional(List<TabelaNutricional> tabelaNutricional) {
+        this.tabelaNutricional = tabelaNutricional;
+    }
 }
