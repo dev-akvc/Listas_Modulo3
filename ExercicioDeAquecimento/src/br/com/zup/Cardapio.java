04419d515c5d8d7fa1cf4a06bbd83d3e2dc59572
Anna Karolyne <dev.akvc@gmail.com>
2021-09-28 23:40:50 -0300

    Classe Cardápio com método toString

--- ExercicioDeAquecimento/src/br/com/zup/Cardapio.java
+++ ExercicioDeAquecimento/src/br/com/zup/Cardapio.java
@@ -1,4 +1,23 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cardapio {
+    private List<Prato> pratos = new ArrayList<>();
+
+    public void adicionarPrato(Prato novoPrato) {
+        pratos.add(novoPrato);
+    }
+
+    @Override
+    public String toString (){
+        StringBuilder pratosDisponiveis = new StringBuilder();
+
+        pratosDisponiveis.append(" * Cardápio *");
+        pratosDisponiveis.append("\n Prato : " +pratos);
+
+        return  pratosDisponiveis.toString();
+    }
+
 }
