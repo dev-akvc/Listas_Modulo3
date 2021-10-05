a663ed3ee2b0fdb6c82bc368a2c8608ba497fd30
Anna Karolyne <dev.akvc@gmail.com>
2021-10-04 22:18:57 -0300

    Ajustar classe Morador como subclasse de Pessoa

--- ListaClasses_Imobiliaria/src/br/com/zup/Morador.java
+++ ListaClasses_Imobiliaria/src/br/com/zup/Morador.java
@@ -1,7 +1,6 @@
 package br.com.zup;
 
-public class Morador {
-
+public class Morador extends Pessoa{
     private String ocupacao;
     private boolean animalDeEstimacao = true;
 
@@ -9,62 +8,14 @@ public class Morador {
     public Morador() {
     }
 
+    public Morador(String ocupacao, boolean animalDeEstimacao) {
+        this.ocupacao = ocupacao;
+        this.animalDeEstimacao = animalDeEstimacao;
+    }
+
     public Morador(String nome, String cpf, String ocupacao, boolean animalDeEstimacao) {
-        this.nome = nome;
-        this.cpf = cpf;
+        super(nome, cpf);
         this.ocupacao = ocupacao;
         this.animalDeEstimacao = animalDeEstimacao;
-    }
-
-    public Morador(String nome, String cpf, String ocupacao) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.ocupacao = ocupacao;
-    }
-
-    //    Getters e setters
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getOcupacao() {
-        return ocupacao;
-    }
-
-    public void setOcupacao(String ocupacao) {
-        this.ocupacao = ocupacao;
-    }
-
-    public boolean isAnimalDeEstimacao() {
-        return animalDeEstimacao;
-    }
-
-    public void setAnimalDeEstimacao(boolean animalDeEstimacao) {
-        this.animalDeEstimacao = animalDeEstimacao;
-    }
-
-    //        Método para exibir os dados do morador
-    @Override
-    public String toString() {
-        StringBuilder exibirMoradores = new StringBuilder();
-
-        exibirMoradores.append("\n Nome: " + nome);
-        exibirMoradores.append("\n CPF: " + cpf);
-        exibirMoradores.append("\n Ocupação: " + ocupacao);
-        exibirMoradores.append("\n Tem animal de estimação? " + animalDeEstimacao);
-
-        return exibirMoradores.toString();
     }
 }
