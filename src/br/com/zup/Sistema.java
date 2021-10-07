2b5bf13ae637022a6b32943ea21c8938f8acb2fc
Anna Karolyne <dev.akvc@gmail.com>
2021-10-07 09:50:51 -0300

    Criar métodos Scanner e menu no Sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,5 +1,19 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
+
+    public static Scanner leitorDados(String mensagem){
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
+
+    public static void menu(){
+        System.out.println(" * Gerenciamento de consumidores * ");
+        System.out.println("\nDigite: \n 1- Cadastrar consumidor \n 2- Cadastrar fatura \n 3- Sair ");
+    }
+
+
 
 }
