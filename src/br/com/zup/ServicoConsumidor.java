4c0779d25115fbbaece447e7f2414ba9315a715d
Anna Karolyne <dev.akvc@gmail.com>
2021-10-07 16:12:49 -0300

    Validar email repetido

--- src/br/com/zup/ServicoConsumidor.java
+++ src/br/com/zup/ServicoConsumidor.java
@@ -6,28 +6,37 @@ import java.util.List;
 public class ServicoConsumidor {
     private static List<Consumidor> consumidores = new ArrayList<>();
 
-    public static void validarEmail(String email) throws Exception{
-        if(!email.contains("@")){
+    public static void validarEmail(String email) throws Exception {
+        if (!email.contains("@")) {
             throw new Exception("* Email inválido *");
         }
     }
 
-    public static Consumidor cadastrarConsumidor(String nome, String email) throws Exception{
+    public static void verificarEmailRepetido(String email) throws Exception {
+        for (Consumidor pesquisarConsumidor: consumidores) {
+            if (pesquisarConsumidor.getEmail().equals(email)){
+                throw new Exception("* Email já cadastrado *");
+            }
+        }
+    }
+
+    public static Consumidor cadastrarConsumidor(String nome, String email) throws Exception {
         validarEmail(email);
+        verificarEmailRepetido(email);
         Consumidor consumidor = new Consumidor(nome, email);
         consumidores.add(consumidor);
         return consumidor;
     }
 
-    public static void exibirConsumidores(){
-        for (Consumidor buscarConsumidor: consumidores) {
+    public static void exibirConsumidores() {
+        for (Consumidor buscarConsumidor : consumidores) {
             System.out.println(buscarConsumidor);
         }
     }
 
-    public static Consumidor pesquisarConsumidorPorEmail (String email) throws Exception{
-        for (Consumidor pesquisarConsumidor: consumidores) {
-            if(pesquisarConsumidor.getEmail().equals(email)){
+    public static Consumidor pesquisarConsumidorPorEmail(String email) throws Exception {
+        for (Consumidor pesquisarConsumidor : consumidores) {
+            if (pesquisarConsumidor.getEmail().equals(email)) {
                 return pesquisarConsumidor;
             }
         }
