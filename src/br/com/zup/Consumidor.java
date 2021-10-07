720652a0345b28941934e2461eb243bcd3d2b962
Anna Karolyne <dev.akvc@gmail.com>
2021-10-07 09:23:54 -0300

    Criar classe Consumidor

--- src/br/com/zup/Consumidor.java
+++ src/br/com/zup/Consumidor.java
@@ -1,4 +1,38 @@
 package br.com.zup;
 
 public class Consumidor {
+    private String nome;
+    private String email;
+
+    public Consumidor() {
+    }
+
+    public Consumidor(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder dadosConsumidor = new StringBuilder();
+        dadosConsumidor.append("Nome do cliente: " +nome);
+        dadosConsumidor.append("Email: " +email);
+        return dadosConsumidor.toString();
+    }
 }
--- src/br/com/zup/Fatura.java
+++ src/br/com/zup/Fatura.java
@@ -1,4 +1,8 @@
 package br.com.zup;
 
 public class Fatura {
+    //Consumidor
+    private double valor;
+    private String dataDeVencimento;
+
 }
