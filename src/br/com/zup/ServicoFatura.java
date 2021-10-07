a74aee7f0dba9484faebf39ead23508784557da9
Anna Karolyne <dev.akvc@gmail.com>
2021-10-07 16:32:42 -0300

    Cadastrar fatura na lista

--- src/br/com/zup/ServicoFatura.java
+++ src/br/com/zup/ServicoFatura.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ServicoFatura {
     private static List<Fatura> faturas = new ArrayList<>();
 
-    public static Fatura cadastrarFatura(String email, double valorFatura, String dataDeVencimento) throws Exception{
+    public static Fatura cadastrarFatura(String email, double valorFatura, String dataDeVencimento) throws Exception {
         Consumidor consumidor = ServicoConsumidor.pesquisarConsumidorPorEmail(email);
 
         Fatura fatura = new Fatura(consumidor, valorFatura, dataDeVencimento);
@@ -14,4 +14,13 @@ public class ServicoFatura {
         return fatura;
     }
 
+    public static List<Fatura> pesquisarFaturaPorEmail(String email) {
+        List<Fatura> faturasDoConsumidor = new ArrayList<>();
+        for (Fatura percorrerFaturas : faturas) {
+            if (percorrerFaturas.getConsumidor().getEmail().equals(email)) {
+                faturasDoConsumidor.add(percorrerFaturas);
+            }
+        }
+        return faturasDoConsumidor;
+    }
 }
--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,31 +1,38 @@
 package br.com.zup;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Sistema {
 
-    public static Scanner leitorDados(String mensagem){
+    public static Scanner leitorDados(String mensagem) {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
 
-    public static void menu(){
+    public static void menu() {
         System.out.println(" * Gerenciamento de consumidores * ");
-        System.out.println("\nDigite: \n 1- Cadastrar consumidor \n 2- Cadastrar fatura \n 3- Sair ");
+        System.out.println("\nDigite: \n 1- Cadastrar consumidor \n 2- Cadastrar fatura \n 3- Consultar faturas de um consumidor \n 4- Sair ");
     }
 
-    public static Consumidor cadastrarConsumidor() throws Exception{
+    public static Consumidor cadastrarConsumidor() throws Exception {
         String nome = leitorDados("Qual nome do consumidor? ").nextLine();
         String email = leitorDados("Qual email?").nextLine();
         return ServicoConsumidor.cadastrarConsumidor(nome, email);
     }
 
-    public static Fatura cadastrarFatura() throws Exception{
+    public static Fatura cadastrarFatura() throws Exception {
         String email = leitorDados("Qual email do consumidor?").nextLine();
         double valorFatura = leitorDados("Informe valor da fatura: ").nextDouble();
         String dataDeVencimento = leitorDados("Informe a data de vecimento: ").nextLine();
 
         return ServicoFatura.cadastrarFatura(email, valorFatura, dataDeVencimento);
+    }
+
+    public static List<Fatura> pesquisarFaturas() {
+        String email = leitorDados("Qual email do consumidor?").nextLine();
+        List<Fatura> faturasDoConsumidor = ServicoFatura.pesquisarFaturaPorEmail(email);
+        return faturasDoConsumidor;
     }
 
 }
