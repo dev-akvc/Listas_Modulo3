f314578eaebedb4fbbdf33826fe43a192246a501
Anna Karolyne <dev.akvc@gmail.com>
2021-10-07 09:43:56 -0300

    Criar listas nas classes de serviço

--- src/br/com/zup/ServicoConsumidor.java
+++ src/br/com/zup/ServicoConsumidor.java
@@ -1,4 +1,9 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicoConsumidor {
+    private static List<Consumidor> consumidores = new ArrayList<>();
+
 }
