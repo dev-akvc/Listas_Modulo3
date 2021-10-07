fe5ea1cc22addaab005e4be5c0b827efad85e338
Anna Karolyne <dev.akvc@gmail.com>
2021-10-07 09:29:25 -0300

    Criar classe Fatura

--- src/br/com/zup/Fatura.java
+++ src/br/com/zup/Fatura.java
@@ -2,7 +2,46 @@ package br.com.zup;
 
 public class Fatura {
     //Consumidor
+    private Consumidor consumidor;
     private double valor;
     private String dataDeVencimento;
 
+    public Fatura(Consumidor consumidor, double valor, String dataDeVencimento) {
+        this.consumidor = consumidor;
+        this.valor = valor;
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public Consumidor getConsumidor() {
+        return consumidor;
+    }
+
+    public void setConsumidor(Consumidor consumidor) {
+        this.consumidor = consumidor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(String dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder dadosFatura = new StringBuilder();
+        dadosFatura.append("Cliente: " +consumidor);
+        dadosFatura.append("Valor da fatura: " +valor);
+        dadosFatura.append("Data de vencimento: " +dataDeVencimento);
+        return dadosFatura.toString();
+    }
 }
